deac24f99465fc6f8f917e1e5e134a6eaf1598ad
darkard2003 <kaushikchowdhury231@gmail.com>
2026-08-09 21:00:04 +0530

    feat: added obs

--- modules/home/programs.nix
+++ modules/home/programs.nix
@@ -7,6 +7,17 @@
 {
   programs.imv.enable = true;
 
+  programs.obs-studio = {
+    enable = true;
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
+
   programs.neovim = {
     enable = true;
     sideloadInitLua = true;
