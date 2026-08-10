45fe89a4ded7b5d28364d51bed3f4473b51efc6f
darkard2003 <kaushikchowdhury231@gmail.com>
2026-08-10 15:30:59 +0530

    feat: added zswap

--- modules/system/zswap.nix
+++ modules/system/zswap.nix
@@ -1,11 +1,11 @@
 { ... }: {
   zramSwap = {
     enable = true;
-    memoryPercent = 100; # Use 100% of RAM as the max compressed pool capacity
-    priority = 999; # Guarantee ZRAM is filled before disk swap
+    memoryPercent = 100;
+    priority = 999;
   };
 
   boot.kernel.sysctl = {
-    "vm.swappiness" = 180; # Proactively compress idle memory
+    "vm.swappiness" = 180;
   };
 }
